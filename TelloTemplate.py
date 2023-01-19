34117578a5e6ba0c730130df8cb6bef0f0f3ed93
Landon Krusniak <Lakrusniak@gnail.com>
2023-01-19 04:36:45 -0800

    1-18-2023

--- TelloTemplate.py
+++ TelloTemplate.py
@@ -75,17 +75,16 @@ try:
 
         sendmsg('command', 0)
         sendmsg("battery?")
-        sendmsg('takeoff')
+        sendmsg('takeoff', 10)
 
-        sendmsg('forward 220', 10)
+        sendmsg('forward 200', 10)
 
-        sendmsg('go 200 0 80 60', 10)
+        sendmsg('go 190 0 80 60', 10)
 
         sendmsg("cw 180", 10)
-        sendmsg("curve -40 -20 0 -250 0 30", 10)
+        sendmsg("curve -25 -25 0 -25 -150 0 30", 10)
 
-
-        sendmsg("go 190 0 -75 40",  10)
+        sendmsg("go 240 0 -75 40", 10)
         sendmsg('land')
 
         print('\nGreat Flight!!!')
